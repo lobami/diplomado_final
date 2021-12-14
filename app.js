92cd7aa20661b1ba496a8abd55d01b843ae652aa
Loth Mejia Martinez <eljugador360@gmail.com>
2021-12-13 20:51:07 -0600

    feat(deploy): add init sentry configuration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var express = require('express');
+const express = require('express');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -27,6 +29,45 @@ app.use('/tasks', tasksRouter);
 app.use('/payment', paymentRouter);
 app.use('/shipment', shipmentRouter);
 app.use('/operations', operationRouter);
+
+
+Sentry.init({
+    dsn: "https://529ce53558a742b39945bf9f78d51a61@o1090184.ingest.sentry.io/6105947",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All controllers should live here
+app.get("/test", function rootHandler(req, res) {
+    res.end("Hello world!");
+    console.log('holiiiii')
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 
 module.exports = app;
