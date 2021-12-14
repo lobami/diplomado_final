6d279c78af83a9ed48fa39ea755ace29b5efb01b
Loth Mejia Martinez <42503437+lobami@users.noreply.github.com>
2021-12-13 21:17:34 -0600

    Devel (#9)
    * feat(keys): updated keys file to test pr
    
    * feat(deploy) update procfile to deploy in heroku
    
    * feat(deploy): delete yarn to deploy
    
    * feat(deploy): update procfile command to deploy
    
    * feat(deploy): add init sentry configuration
    
    * fix(deploy): fix init sentry configuration

--- app.js
+++ app.js
@@ -14,9 +14,45 @@ var operationRouter = require('./routes/operations');
 
 var app = express();
 
+Sentry.init({
+    dsn: "https://529ce53558a742b39945bf9f78d51a61@o1090184.ingest.sentry.io/6105947",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All controllers should live here
+app.get("/test", function rootHandler(req, res) {
+    res.end("Hello world!");
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
+
 // view engine setup
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +65,12 @@ app.use('/tasks', tasksRouter);
 app.use('/payment', paymentRouter);
 app.use('/shipment', shipmentRouter);
 app.use('/operations', operationRouter);
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
 
 
 Sentry.init({
