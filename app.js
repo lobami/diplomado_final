b030f422466ea838857b6fd1fbe9a1b3e41b5311
Loth Mejia Martinez <42503437+lobami@users.noreply.github.com>
2021-12-13 22:01:50 -0600

    Devel (#10)
    * feat(keys): updated keys file to test pr
    
    * feat(deploy) update procfile to deploy in heroku
    
    * feat(deploy): delete yarn to deploy
    
    * feat(deploy): update procfile command to deploy
    
    * feat(deploy): add init sentry configuration
    
    * fix(deploy): fix init sentry configuration
    
    * fix(monitoring): init datadog

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var authRouter = require('./routes/auth');
 var paymentRouter = require('./routes/payment');
 var shipmentRouter = require('./routes/shipment');
 var operationRouter = require('./routes/operations');
+var dd_options = {
+    'response_code':true,
+    'tags': ['app:diplomado-final']
+  }
+var connect_datadog = require('connect-datadog')(dd_options);
 
 var app = express();
 
@@ -73,43 +78,5 @@ app.use(Sentry.Handlers.errorHandler());
 
 
 
-Sentry.init({
-    dsn: "https://529ce53558a742b39945bf9f78d51a61@o1090184.ingest.sentry.io/6105947",
-    integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Tracing.Integrations.Express({ app }),
-    ],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-});
-
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
-
-// All controllers should live here
-app.get("/test", function rootHandler(req, res) {
-    res.end("Hello world!");
-    console.log('holiiiii')
-});
-
-// The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-    // The error id is attached to `res.sentry` to be returned
-    // and optionally displayed to the user for support.
-    res.statusCode = 500;
-    res.end(res.sentry + "\n");
-});
-
-
+app.use(connect_datadog);
 module.exports = app;
